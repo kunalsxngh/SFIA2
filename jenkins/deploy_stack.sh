70f292949e5376a9c010be25d7631d745614a6d3
kunalsxngh <ksingh143@hotmail.com>
2021-01-20 17:06:42 +0000

    updating Jenkinsfile to work with ansible

--- jenkins/deploy_stack.sh
+++ jenkins/deploy_stack.sh
@@ -2,5 +2,5 @@
 scp -i ~/.ssh/id_rsa docker-compose.yaml jenkins@swarm-master:/home/jenkins/docker-compose.yaml
 ssh -i ~/.ssh/id_rsa jenkins@swarm-master << EOF
     export DATABASE_URI=${DATABASE_URI}
-    docker stack deploy --compose-file /home/jenkins/docker-compose.yaml
+    docker stack deploy --compose-file /home/jenkins/docker-compose.yaml numberplate-stack
 EOF
