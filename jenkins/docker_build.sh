22b032cb9b3aa410ff53f108a6f4d44caaa1053b
kunalsxngh <ksingh143@hotmail.com>
2021-01-21 14:49:18 +0000

    jenkinsfile

--- jenkins/docker_build.sh
+++ jenkins/docker_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker login -u= "$docker_credentials.USR" -p="$docker_credentials.PSW"
+docker login -u $docker_credentials.USR -p $docker_credentials.PSW
 /usr/local/bin/docker-compose build
 /usr/local/bin/docker-compose push
