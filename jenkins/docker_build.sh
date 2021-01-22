41aa1df926599e9e9ae551ea19f9fc84f1eda2d4
kunalsxngh <ksingh143@hotmail.com>
2021-01-22 14:50:13 +0000

    Making Docker build more efficient

--- jenkins/docker_build.sh
+++ jenkins/docker_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker login -u $docker_credentials_USR -p $docker_credentials_PSW
-/usr/local/bin/docker-compose build
+/usr/local/bin/docker-compose build --parallel
 /usr/local/bin/docker-compose push
