19ea244559f0d01b8edd08c18e7c517928a11170
kunalsxngh <ksingh143@hotmail.com>
2021-01-22 14:14:17 +0000

    Making testing more efficient

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -3,8 +3,8 @@
 
 python3 -m venv venv
 source venv/bin/activate
-pip3 install -r requirements.txt
-pip3 install pytest pytest-cov flask_testing requests_mock
+pip3 install pytest pytest-cov flask_testing requests_mock flask_sqlalchemy
+pip3 install -r service_1/requirements.txt
 
 for i in 1 2 3 4
 do
