b8e4fa412de55a2f480c70acb81c58432b0ab616
kunalsxngh <ksingh143@hotmail.com>
2021-01-22 14:11:28 +0000

    Making testing more efficient

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Test service i
 
-#!/bin/bash
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+pip3 install pytest pytest-cov flask_testing requests_mock
+
 for i in 1 2 3 4
 do
     cd service_$i
-    python3 -m venv venv
-    source venv/bin/activate
-    pip3 install -r requirements.txt
-    pip3 install pytest pytest-cov flask_testing requests_mock
     python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
-    deactivate
     cd ..
 done
+deactivate
