e6735d616115000e5b8c7366a08f346965c95c55
kunalsxngh <ksingh143@hotmail.com>
2021-01-23 21:55:00 +0000

    Adding versions to images

--- service_4/tests/test_app.py
+++ service_4/tests/test_app.py
@@ -17,14 +17,14 @@ class TestResponse(TestBase):
         data = {"letters" : 'ABCDE', "numbers" : '15'}
         response = self.client.post(url_for('get_numberplate'), json=data)
         json_response = response.data
-        test_json = b'{"numberplate": "AB15CDE", "color": "red"}'
+        test_json = b'{"numberplate": "AB15CDE", "color": "blue"}'
         assert json_response == test_json
 
         with patch('random.randrange') as random:
             random.return_value = "5"
             data = {"letters" : 'ABCDG', "numbers" : '9'}
             response = self.client.post(url_for('get_numberplate'), json=data)
-            test_json = b'{"numberplate": "AB14CDG", "color": "red"}'
+            test_json = b'{"numberplate": "AB14CDG", "color": "blue"}'
         
         data = {"letters" : 'ABCDE', "numbers" : '4'}
         response = self.client.post(url_for('get_numberplate'), json=data)
