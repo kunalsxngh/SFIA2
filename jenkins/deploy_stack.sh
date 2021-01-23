6b23d062d8709393e18db12ed97a656bc20cbe1c
kunalsxngh <ksingh143@hotmail.com>
2021-01-23 21:50:55 +0000

    Adding versions to images

--- jenkins/deploy_stack.sh
+++ jenkins/deploy_stack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 scp -i ~/.ssh/id_rsa docker-compose.yaml jenkins@swarm-master:/home/jenkins/docker-compose.yaml
 ssh -i ~/.ssh/id_rsa jenkins@swarm-master << EOF
-    export DATABASE_URI=${DATABASE_URI}
+    export DATABASE_URI=${DATABASE_URI} app_version=${app_version} replicas=${replicas}
     docker stack deploy --compose-file /home/jenkins/docker-compose.yaml numberplate-stack
 EOF
